fe97a99e7b8ed28a5c9bfa63c9475d472da04e34
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-07-14 10:42:06 -0700

    scrolling event to navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  @HostListener("window:scroll", ['$event']) onScrollEvent(event: any) {
+    console.log(event);
+    console.log('scrolling')
+  }
   constructor() { }
 
   ngOnInit(): void {
