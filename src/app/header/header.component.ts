154fd4e5f2dcc0d6e549036bf878e7490bc17c57
Kelton Hale <77764287+khale913@users.noreply.github.com>
2022-07-14 11:26:59 -0700

    navbar fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core';
+import { windowCount } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +8,16 @@ import { HostListener } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @HostListener("window:scroll", ['$event']) onScrollEvent(event: any) {
-    console.log(event);
-    console.log('scrolling')
+    // console.log(event);
+    // console.log(window.pageYOffset)
+    console.log(window.scrollY)
+    if (window.pageYOffset >= 45 || window.scrollY >= 45) {
+      this.lessPadding = true;
+    } else {
+      this.lessPadding = false;
+    }
   }
+  lessPadding: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
